b4c63ec61b2c516f7baff3d2f1cbee144c295845
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-04-25 22:13:24 -0400

    moved database folder into server folder

--- server/app.js
+++ server/app.js
@@ -3,12 +3,14 @@ const morgan = require('morgan')
 const compression = require('compression')
 const router = require('./routes')
 const cors = require('cors')
+const helmet = require('helmet')
 const app = express()
 
 require('dotenv').config()
 
 app.use(compression())
 app.use(morgan('dev'))
+app.use(helmet({}))
 app.use(cors())
 app.use('/products', router)
 
